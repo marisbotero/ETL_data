7b4bda4802ab7c7da8799eff886d32577a37ef5b
marisbotero <maris2143@gmail.com>
2019-04-19 09:25:54 -0500

    💛

---
+++ newspaper_recepie.py
@@ -0,0 +1,23 @@
+
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
+from urllib.parse import urlparse
+
+import pandas as pd
+
+
+
+logger = logging.getLogger(__Name__)
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename',
+    hel
+    )
+    main()
