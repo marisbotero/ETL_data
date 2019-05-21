57fb37c004cc45e4e5781486d9a7b49266c97da0
marisbotero <maris2143@gmail.com>
2019-05-20 21:32:33 -0500

    🧡

--- report/main.py
+++ report/main.py
@@ -1,2 +1,25 @@
-if __name__ == "__main__":
-    pass
+import pandas as pd
+import numpy as np
+
+# Bokeh libraries
+from bokeh.io import output_file, output_notebook
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource
+from bokeh.layouts import row, column, gridplot
+from bokeh.models.widgets import Tabs, Panel
+
+# Prepare the data
+
+# Determine where the visualization will be rendered
+output_file('filename.html')  # Render to static HTML, or 
+output_notebook()  # Render inline in a Jupyter Notebook
+
+# Set up the figure(s)
+fig = figure()  # Instantiate a figure() object
+
+# Connect to and draw the data
+
+# Organize the layout
+
+# Preview and save 
+show(fig)  # See what I made, and save if I like it
